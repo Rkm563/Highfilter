6a63a06e11c897ff0d140c62b1925cbc012806b6
Rkm563 <147932740+Rkm563@users.noreply.github.com>
2023-11-12 12:22:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Highfilter 
 fi
-cd /VJ-FILTER-BOT 
+cd /Highfilter 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
