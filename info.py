8bb2d0f93f8c022992c679e90fa1c393b7fa6dca
Rkm563 <147932740+Rkm563@users.noreply.github.com>
2023-12-29 16:43:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -105,8 +105,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://high-filter.onrender.com/".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://high-filter.onrender.com/".format(FQDN, PORT)
+URL = "https://high-filter.onrende.com/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://high-filter.onrende.com/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -121,9 +121,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://high-filter.onrender.com/".format(FQDN)
+    URL = "https://high-filter.onrende.com/".format(FQDN)
 else:
-    URL = "https://high-filter.onrender.com/".format(FQDN)
+    URL = "https://high-filte.onrender.com/".format(FQDN)
 
 
 
