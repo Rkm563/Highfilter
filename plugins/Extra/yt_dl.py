cdc6724c90f896c9deb476860924fcb8ae13974f
Rkm563 <147932740+Rkm563@users.noreply.github.com>
2024-01-24 17:38:51 +0530

    Update yt_dl.py

--- plugins/Extra/yt_dl.py
+++ plugins/Extra/yt_dl.py
@@ -44,11 +44,11 @@ async def song(client, message):
     await m.edit("**dσwnlσαdíng чσur ѕσng...!**")
     try:
         with YoutubeDL(ydl_opts) as ydl:
-            info_dict = ydl.extract_info(link, download=False)
+            info_dict = ydl.extract_info(link, download=True)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
 
-        cap = "**BY›› [VJ NETWORKS™](https://t.me/vj_bots)**"
+        cap = "**BY›› [VJ NETWORKS™](https://t.me/FASTMOVIEDOWNLOADS)**"
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
