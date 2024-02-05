37618437f430f87eff7c1facca33f75e5ebc6e1a
Rkm563 <147932740+Rkm563@users.noreply.github.com>
2024-02-05 11:53:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -55,7 +55,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 # Others
 VERIFY = bool(environ.get('VERIFY', True))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'omegalinks.in')
-SHORTLINK_API = environ.get('SHORTLINK_API', '1230ccb1db8a90b7ff48819187a4c5b56e58e3bb')
+SHORTLINK_API = environ.get('SHORTLINK_API', '472768573cda2f60ef3e6919e17ec6d4a39e460e')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
